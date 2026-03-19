4c7dab578588c7affa4a247d87ab9174bd93f962
JaidaT <jtarlit3@my.bcit.ca>
2026-03-18 17:45:43 -0700

    Add images to info box depending on the type of algae being hovered over

--- algaeworldapp/components/AlgaeInfoOverlay/index.js
+++ algaeworldapp/components/AlgaeInfoOverlay/index.js
@@ -13,7 +13,24 @@ const algaeInfo = {
   "Benthic Mats": "Produces harmful toxins that may affect other plants and animals.",
 };
 
+const algaeImageSrc = {
+  "Carpet Algae": "/images/surface_algae/Carpet Algae.svg",
+  "Chlorella": "/images/surface_algae/Chlorella.svg",
+  "Spirogyra": "/images/surface_algae/Spirogyra.svg",
+  "Bubble Algae": "/images/floor_algae/Bubble Algae.svg",
+  "Gracilaria": "/images/floor_algae/Gracilaria.svg",
+  "Gulfweed": "/images/floor_algae/Gulfweed.svg",
+  "Peacock's Tail": "/images/floor_algae/Peacock's Tail.svg",
+  "Sea Lettuce": "/images/floor_algae/Sea Lettuce.svg",
+  "Dinoflagellates": "/images/surface_algae/Dinoflagellates.svg",
+  "Cyanobacteria": "/images/surface_algae/Cyanobacteria.svg",
+  "Grape Algae": "/images/floor_algae/Grape Algae.svg",
+  "Benthic Mats": "/images/floor_algae/Benthic Mats.svg",
+};
+
 export default function AlgaeInfoOverlay({ hoveredAlgae }) {
+  const imageSrc = algaeImageSrc[hoveredAlgae];
+
   return (
     <div
       style={{
@@ -46,6 +63,19 @@ export default function AlgaeInfoOverlay({ hoveredAlgae }) {
           height: "100%",
         }}
       >
+        {imageSrc && (
+          <img
+            src={imageSrc}
+            style={{
+              position: "absolute",
+              top: "52px",
+              left: "38%",
+              width: "70px",
+              height: "70px",
+              objectFit: "contain",
+            }}
+          />
+        )}
         <div
           style={{
             fontWeight: "bold",
